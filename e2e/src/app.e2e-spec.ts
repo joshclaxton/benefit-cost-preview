f329bfbf7b14c7f098c8002717dc1848c4d2651b
Joshua Claxton <joshclaxton@live.com>
2020-04-12 14:45:04 -0700

    Fix e2e app title test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to benefit-cost-preview!');
+    expect(page.getTitleText()).toEqual('Benefit Cost Preview');
   });
 
   afterEach(async () => {
