633e729ba44a5f3fca9c61c58c6e859fde7a1154
Joshua Claxton <joshclaxton@live.com>
2020-04-16 19:11:14 -0700

    Wrap Angular Project with C# Server

---
+++ BenefitCostPreview/Controllers/BenefitCost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BenefitCostPreview.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BenefitCost : ControllerBase
+    {
+        private readonly ILogger<BenefitCost> _logger;
+
+        public BenefitCost(ILogger<BenefitCost> logger)
+        {
+            _logger = logger;
+        }
+
+        // todo
+        [HttpGet]
+        public void Get()
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
